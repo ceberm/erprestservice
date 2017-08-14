797169535b9d9b0a61331fca9c53c3cef141a5df
ceberm <c.bg@live.com>
2017-08-14 17:18:33 -0600

    First Commit

---
+++ api/controllers/ERPController.js
@@ -0,0 +1,78 @@
+'use strict'
+
+var mongoose = require('mongoose'),
+Task = mongoose.model('Tasks');
+
+exports.list_all_tasks = function(req, res){
+	Task.find({}, function(err,task){
+		if (err)
+			res.semnd(err);
+		res.json(task);
+	});
+};
+
+
+
+exports.create_a_task = function(res.res){
+	ar new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.update_a_task = function(req, res) {
+  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.delete_a_task = function(req, res) {
+
+
+  Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
